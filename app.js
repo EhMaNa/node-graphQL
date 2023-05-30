0f2a2a80cf80ec067df61cf196d4f80613da4794
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2023-05-30 08:30:34 +0000

    update: setting up graphql

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const app = express();
+const { createHandler } = require('graphql-http/lib/use/express');
+
+
+const port = process.env.PORT || 3000;
+app.listen(port);
