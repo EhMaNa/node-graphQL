ddad826c5905398b1f6fe1d0d8b5217fc190dbb7
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2023-05-31 12:26:24 +0000

    update: setting up mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
+const mongo = require('mongoose');
 
 const GraphQLSchema = require('./schema/schema')
 
@@ -11,5 +12,16 @@ app.use(
       graphiql: true,
     }),
   );
+
+  const db = process.env.MONGO_URI
+  mongo.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+          console.log('Connected Successfully To ' + db)
+      })
+      .catch(() => {
+          console.log('An Error Occured Whiles Connecting To Database')
+      })
+
+
 const port = process.env.PORT || 3000;
 app.listen(port);
